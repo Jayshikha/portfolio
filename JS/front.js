63bbe45307ef79020a44d3f701ddbb06867d5342
jayshikha <njayshikha@gamil.com>
2023-07-28 15:36:11 +0530

    all set with project

--- JS/front.js
+++ JS/front.js
@@ -1,21 +1,24 @@
 document.querySelector('.cross').style.display = 'none';
 document.querySelector('.ham').addEventListener("click", () => {
    document.querySelector('.sidebar').classList.toggle('sideBarGo');
-   // if (document.querySelector('.sidebar').classList.contains('.sideBarGo'))
-   //  {
-   //    document.querySelector('.fa-bars').style.display = 'inline';
-   //    // setTimeout(() => {
-   //    //    document.querySelector('.cross').style.display = 'none';
-         
-   //    // }, 350);
-   // }
-   // else
-   //  {
-   //    document.querySelector('.fa-bars').style.display = 'none'
-   //    //  setTimeout(() => {
-
-      //    document.querySelector('.cross').style.display = 'inline'
-      // }, 350);
-   // }
+   
 })
 
+
+
+document.querySelector('#submit').addEventListener("click",()=>{
+ let userName = document.getElementById("Inputetail");
+let userNum = document.getElementById("Inputnumber");
+let userNameVal = userName.value;
+let userNumVal = userNum.value;
+
+   let conf1 = confirm(`Your Email is ${userNameVal}`);
+   let conf2 = confirm(`Your Contect Number is ${userNumVal}`);
+   
+   
+   if (conf1 == true && conf2 == true ){
+    alert("We Got Your Message Successfully :)")
+     
+  }
+   
+})
